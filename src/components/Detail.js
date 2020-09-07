5f2fd6dd4da246075fc11e49fba9cb824e48cd5f
Laila <laila.lahbal@ikaconseil.com>
2020-09-07 11:17:47 +0200

    L

---
+++ src/components/Detail.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import Title from './Title';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+class Detail extends React.Component {
+    constructor(props) {
+        super(props);
+      
+   
+    }
+
+  
+
+    render() {
+
+        return (<div></div>)
+    }
+}
+
+export default Detail;
